a24753fc213da8aaf3a9ad925c63d05c64761239
Ruben Espinoza Osses <r.espinoza05@ufromail.cl>
2021-06-01 16:39:16 -0400

    Termine la prueba

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/" replace>imagen</router-link> |
-      <router-link to="/peliculas/populares" replace>Populares</router-link> |
-      <router-link to="/peliculas/top-rated" replace
-        >Mejor Puntuadas</router-link
-      >
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <router-link class="elem-at-right mr-sm-2" to="/" replace>Home</router-link> |
+        <router-link class="elem-at-right mr-sm-2" to="/peliculas/populares" replace>Populares</router-link> |
+        <router-link class="elem-at-right mr-sm-2" to="/peliculas/top-rated" replace>Mejor Puntuadas</router-link>
+      </nav>
     </div>
     <router-view />
   </div>
 </template>
 
 <script>
-
 </script>
+<style>
+</style>
 
 
 <style>
+
+.elem-at-right{
+  align-content: right;
+
+  padding: 10px;
+}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
